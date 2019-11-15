754966af3d4f802ecbe62687d08b91940bd6af1e
Antoine Sample <a.sample@f5.com>
2019-11-15 12:26:21 -0800

    Added multiple math functions

--- Javascript Directory/javascript/learn-to-code.js
+++ Javascript Directory/javascript/learn-to-code.js
@@ -3,5 +3,13 @@ var age = 23;
 var message = "Hi, my name is " +name +" and I am " + age +"years old.";
 var aged = 23;
 var sum = 10 + 15
+var sub = 15-10;
+var mult = 10*30;
+var div = 10/3;
+var mod = 10 % 3;
+var msg = "10 divided by 3 equals 3 with a remainder of" + mod;
 console.log(message);
 console.log(sum);
+console.log(sub);
+console.log(mult);
+console.log(msg);
