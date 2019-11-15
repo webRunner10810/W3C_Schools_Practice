1f05360aa10f6c3f6f84c63351b2140f080ca90f
Antoine Sample <a.sample@f5.com>
2019-11-15 12:21:39 -0800

    Math function added to js

--- Javascript Directory/javascript/learn-to-code.js
+++ Javascript Directory/javascript/learn-to-code.js
@@ -1,4 +1,7 @@
 var name="Jack";
 var age = 23;
 var message = "Hi, my name is " +name +" and I am " + age +"years old.";
+var aged = 23;
+var sum = 10 + 15
 console.log(message);
+console.log(sum);
